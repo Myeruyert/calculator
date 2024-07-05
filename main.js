ae8b9a9bc1a18fb5345b37d01bfad7c0b509a7fd
Myeruyert <myeruyertjanibyek@gmail.com>
2024-07-05 12:20:42 +0800

    done

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const numberBoard = document.getElementsByClassName("inside")[0];
-const button = document.querySelectorAll(".btn");
+const button = document.querySelectorAll(".btn-num");
 const plus = document.getElementsByClassName("plus")[0];
+const opsBtns = document.getElementsByClassName("op");
+const multiple = document.getElementsByClassName("multiple")[0];
+const divide = document.getElementsByClassName("divide")[0];
+const equal = document.getElementsByClassName("equal")[0];
 
 // const show = [];
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -19,16 +23,43 @@ const plus = document.getElementsByClassName("plus")[0];
 // twoEl.addEventListener("click", () => {
 //   numberBoard.innerHTML = 2;
 // });
-
+let a = 0;
+let operator = "";
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("click", (event) => {
     // console.log("eventt", event.target.textContent);
     numberBoard.textContent += event.target.textContent;
     // console.log(numberBoard.textContent);
-    let convertToNumber = Number(numberBoard.textContent);
-    console.log("convert", convertToNumber);
-    // add(prevValue, current, operator)
   });
 }
 
-plus.addEventListener("click", () => {});
+for (let i = 0; i < opsBtns.length; i++) {
+  opsBtns[i].addEventListener("click", () => {
+    a = Number(numberBoard.textContent);
+    console.log("a", typeof a);
+    numberBoard.textContent = "";
+    operator = opsBtns[i].textContent;
+    console.log("op", operator);
+  });
+}
+
+equal.addEventListener("click", () => {
+  let res = 0;
+  if (operator === "+") {
+    res = a + Number(numberBoard.textContent);
+    console.log("res", res);
+  }
+  if (operator === "-") {
+    res = a - Number(numberBoard.textContent);
+    console.log("res", res);
+  }
+  if (operator === "*") {
+    res = a * Number(numberBoard.textContent);
+    console.log("res", res);
+  }
+  if (operator === "/") {
+    res = a / Number(numberBoard.textContent);
+    console.log("res", res);
+  }
+  numberBoard.textContent = res;
+});
